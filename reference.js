aa11c619c3c5c169f24de72a9767e4c107671a76
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-07-20 03:26:09 +0100

    working with NaN

--- reference.js
+++ reference.js
@@ -1,9 +1,10 @@
+log = console.log;
 // globalThis
 function canMakeHTTPRequest() {
     return typeof globalThis.XMLHttpRequest === 'function';
 }
 
-console.log(canMakeHTTPRequest());
+// console.log(canMakeHTTPRequest());
 
 function check(it) {
     // Math is known to exist as a global in every environment.
@@ -45,13 +46,48 @@ if (maxNumber === Infinity) {
     // Expected output: "Let's call it infinity!"
 }
 
-console.log(1 / maxNumber);
+// console.log(1 / maxNumber);
 // Expected output: 0
 
 // Using Infinity
-console.log(Infinity);
-console.log(Infinity + 1);
-console.log(Math.pow(10, 1000));
-console.log(Math.log(0));
-console.log(1 / Infinity);
-console.log(1 / 0);
+// console.log(Infinity);
+// console.log(Infinity + 1);
+// console.log(Math.pow(10, 1000));
+// console.log(Math.log(0));
+// console.log(1 / Infinity);
+// console.log(1 / 0);
+
+
+// NaN
+function sanitise(x) {
+    if (isNaN(x)) {
+        return NaN;
+    }
+    return x;
+}
+// console.log(sanitise('1'));
+// Expected output: "1"
+// console.log(sanitise('NotANumber'));
+// Expected output: NaN
+
+// Testing against NaN
+NaN === NaN; // false
+Number.NaN === NaN;
+
+function valueIsNaN(v) {
+    return v !== v;
+}
+// log(valueIsNaN(1));
+// log(valueIsNaN(NaN));
+// log(valueIsNaN(Number.NaN));
+
+
+// Undefined
+function test(t) {
+    if (t === undefined) {
+        return 'Undefined value!';
+    }
+    return t;
+}
+let x;
+log(test(x))
